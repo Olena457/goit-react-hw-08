4ec4f722686baae61faf7909ade28bd2e023ae5a
Olena457 <olenademianenko42@gmail.com>
2024-06-26 18:45:32 +0300

    corected errors1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import { useAuth } from '../hooks';
 const HomePage = lazy(() => import('../pages/HomePage'));
 const RegistrationPage = lazy(() => import('../pages/RegistrationPage'));
 const LoginPage = lazy(() => import('../pages/Login'));
-const TasksPage = lazy(() => import('../pages/Tasks'));
+const ContactsPage = lazy(() => import('../pages/ContactsPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -44,7 +44,7 @@ export const App = () => {
         <Route
           path="/tasks"
           element={
-            <PrivateRoute redirectTo="/login" component={<TasksPage />} />
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
           }
         />
       </Route>
