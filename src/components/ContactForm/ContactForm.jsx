49913be7bd7542e041f9890208f5c778815489e3
Olena457 <olenademianenko42@gmail.com>
2024-07-02 18:40:10 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,7 +4,7 @@ import SearchBox from '../SearchBox/SearchBox';
 import css from './ContactForm.module.css';
 import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
-import { addContact } from '../../redux/contactsOps';
+import { addContact } from '../../redux/contacts/operations';
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
